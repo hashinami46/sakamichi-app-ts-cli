2d568756710659aa0d6e30978c0bfbda78398b7d
hashinami46 <hashinami46@gmail.com>
2023-09-10 11:40:02 +0900

    🎥 Fix download logic and add custom download path

--- .deps/Downloader.py
+++ .deps/Downloader.py
@@ -1,90 +1,89 @@
-X='sakukoi_card'
+Y='sakukoi_card'
 H='itsunogi_sprites'
 G='nogifes_card'
-W='Texture2D'
-V='GET'
-U=bytes
-T=range
-O='movie'
-N=exit
-L=len
+X='Texture2D'
+W='GET'
+V=bytes
+U=range
+Q='movie'
+P=exit
+N=len
 E=int
-C=print
-import os as D,re,json as R,httpx as M,UnityPy as S,argparse as I
+D=print
+import os as B,re,json as S,httpx as O,UnityPy as T,argparse as I
 from PIL import Image as F
 from PyCriCodecs import CPK,USM,ACB
 def J(infile,outdir,key):USM(infile,key=key).extract(dirname=outdir)
-def K(infile,outdir,key):A=outdir;CPK(infile).extract();USM(O,key=key).extract(dirname=A);ACB('music').extract(dirname=A,decode=True,key=key)
-def P(asset_type,infile,outfile):
+def K(infile,outdir,key):A=outdir;CPK(infile).extract();USM(Q,key=key).extract(dirname=A);ACB('music').extract(dirname=A,decode=True,key=key)
+def L(asset_type,infile,outfile):
 	A=outfile
-	with M.stream(V,infile,timeout=None)as C:
-		D=S.load(C.read())
+	with O.stream(W,infile,timeout=None)as C:
+		D=T.load(C.read())
 		for B in D.objects:
-			if B.type.name==W:B.read().image.save(A)
+			if B.type.name==X:B.read().image.save(A)
 			if asset_type not in[G,H]:
 				with F.open(A)as E:E.resize((900,1200),resample=F.LANCZOS).save(A)
-def Q(asset_type,member_data,catalog,path_server,path_local,from_index=0,to_index=0):
-	d='assetBundleName';c='fileSize';b='location';a='card';Y=path_local;P=catalog;J=to_index;I='data';H='/';B=from_index;F=asset_type;Z=path_server.split(H);Z[-2]=P.split('_')[2]
-	def Q(filename):
-		c='gen';b='content-length';G=filename
-		with M.stream(V,H.join(Z)+H+G,timeout=None)as J:
-			if J.status_code!=200:C('\x1b[38;5;1mWhoops, server error\x1b[0m');N(1)
-			I=bytearray(J.read());L=I[7]
-			for P in T(150):I[P]^=L
-			Q=S.load(U(I))
-			if Q.objects and a in F:
-				for B in Q.objects:
-					d='\\d{7}_'if F==X else'\\d{8}_\\d'
-					if B.type.name==W and 100000<=E(J.headers[b])<=1000000 and re.match(d,B.read().name):
-						for K in R.loads(member_data):
-							if B.read().name[3:6]==K[c]+K[F.split('_')[0]]:
-								A=f"{Y}/{K[c]}. {K['name']}"
-								if not D.path.exists(A):D.makedirs(A)
-								if D.path.exists(f"{A}/{B.read().name}.png"):C(f"[38;5;11m{B.read().name} already exist[0m")
-								if not D.path.exists(f"{A}/{B.read().name}.png"):B.read().image.save(f"{A}/{B.read().name}.png");C(f"{B.read().name} saved!")
-			elif not Q.objects and O in F:
-				if E(J.headers[b])>=1000000:
-					A=f"{Y}"
-					if not D.path.exists(A):D.makedirs(A)
-					if D.path.exists(f"{A}/{G.split(H)[1]}.mp4"):C(f"[38;5;11m{G} already exists![0m")
-					if not D.path.exists(f"{A}/{G.split(H)[1]}.mp4"):
-						L=I[15]
-						for P in T(150):I[P]^=L
-						with open(f"{A}/{G.split(H)[1]}.mp4",'wb')as e:e.write(U(I))
-						C(f"{G}.mp4 Downloaded!")
-		J.close()
-	e=M.Client()
-	with open(P,'rb')as f:G=R.load(f)
-	if B>L(G[I])or J>L(G[I]):C('\x1b[38;5;1mWhoops, an error occured!\x1b[0m');N(1)
-	C(f"Data length : {str(L(G[I]))}")
-	if B==0 or J==0:K=G[I]
-	if B!=0 and J!=0:K=G[I][B:J]
-	if B!=0 and J==0:K=G[I][B:]
-	C(f"Total requested range: {str(L(K))}")
-	if a in F:
-		for A in K:
-			if A[b]==2 and 100000<=E(A[c])<=1000000:Q(A[d])
-	elif O in F:
-		for A in K:
-			if A[b]==2 and E(A[c])>=1000000:Q(A[d])
+def M(asset_type,member_data,catalog,path_server,path_local,from_index=0,to_index=0):
+	d='fileSize';c='location';b='card';R=catalog;M='assetBundleName';K=to_index;J='data';H=from_index;G=path_local;F=asset_type;C='/';Z=path_server.split(C);Z[-2]=R.split('_')[2]
+	def a(filename):
+		c='gen';a='content-length';L=filename
+		with O.stream(W,C.join(Z)+C+L,timeout=None)as I:
+			if I.status_code!=200:D('\x1b[38;5;1mWhoops, server error\x1b[0m');P(1)
+			H=bytearray(I.read());M=H[7]
+			for N in U(150):H[N]^=M
+			R=T.load(V(H))
+			if R.objects and b in F:
+				for A in R.objects:
+					d='\\d{7}_'if F==Y else'\\d{8}_\\d'
+					if A.type.name==X and 100000<=E(I.headers[a])<=1000000 and re.match(d,A.read().name):
+						for K in S.loads(member_data):
+							if A.read().name[3:6]==K[c]+K[F.split('_')[0]]:
+								J=B.path.join(G,f"{K[c]}. {K['name']}")
+								if not B.path.exists(J):B.makedirs(J)
+								if B.path.exists(B.path.join(J,f"{A.read().name}.png")):D(f"[38;5;11m{A.read().name} already exist[0m")
+								if not B.path.exists(B.path.join(J,f"{A.read().name}.png")):A.read().image.save(B.path.join(J,f"{A.read().name}.png"));D(f"{A.read().name} saved!")
+			elif not R.objects and Q in F:
+				if E(I.headers[a])>=1000000:
+					if not B.path.exists(G):B.makedirs(G)
+					M=H[15]
+					for N in U(150):H[N]^=M
+					with open(B.path.join(G,f"{L.split(C)[1]}.mp4"),'wb')as e:e.write(V(H))
+					D(f"{L.split(C)[1]}.mp4 Downloaded!")
+		I.close()
+	e=O.Client()
+	with open(R,'rb')as f:I=S.load(f)
+	if H>N(I[J])or K>N(I[J]):D('\x1b[38;5;1mWhoops, an error occured!\x1b[0m');P(1)
+	D(f"Data length : {str(N(I[J]))}")
+	if H==0 or K==0:L=I[J]
+	if H!=0 and K!=0:L=I[J][H:K]
+	if H!=0 and K==0:L=I[J][H:]
+	D(f"Total requested range: {str(N(L))}")
+	if b in F:
+		for A in L:
+			if A[c]==2 and 100000<=E(A[d])<=1000000:a(A[M])
+	elif Q in F:
+		for A in L:
+			if A[c]==2 and E(A[d])>=1000000:
+				if B.path.exists(B.path.join(G,f"{A[M].split(C)[1]}.mp4")):D(f"[38;5;11m{A[M].split(C)[1]} already exists![0m")
+				if not B.path.exists(B.path.join(G,f"{A[M].split(C)[1]}.mp4")):a(A[M])
 	e.close()
-B=I.ArgumentParser(description='Downloader Helper')
-B.add_argument('--type')
-B.add_argument('--infile')
-B.add_argument('--outdir')
-B.add_argument('--key')
-B.add_argument('--outfile')
-B.add_argument('--memberdata')
-B.add_argument('--catalog')
-B.add_argument('--pathserver')
-B.add_argument('--pathlocal')
-B.add_argument('--fromindex')
-B.add_argument('--toindex')
-A=B.parse_args()
+C=I.ArgumentParser(description='Downloader Helper')
+C.add_argument('--type')
+C.add_argument('--infile')
+C.add_argument('--outdir')
+C.add_argument('--key')
+C.add_argument('--outfile')
+C.add_argument('--memberdata')
+C.add_argument('--catalog')
+C.add_argument('--pathserver')
+C.add_argument('--pathlocal')
+C.add_argument('--fromindex')
+C.add_argument('--toindex')
+A=C.parse_args()
 if __name__=='__main__':
 	if A.type in['nogifes_movie_card','nogifes_reward_movie']:J(A.infile,A.outdir,E(A.key))
 	elif A.type in['nogifes_focus_data_lo','nogifes_focus_data_hi']:K(A.infile,A.outdir,E(A.key))
-	elif A.type in[G,H,'itsunogi_card','itsunogi_photo']:P(A.type,A.infile,A.outfile)
-	elif A.type in[X,'hinakoi_card','sakukoi_movie','hinakoi_movie']:
-		try:Q(A.type,A.memberdata,A.catalog,A.pathserver,A.pathlocal,E(A.fromindex),E(A.toindex))
-		except KeyboardInterrupt:C('Process Cancelled by User');N(1)
+	elif A.type in[G,H,'itsunogi_card','itsunogi_photo']:L(A.type,A.infile,A.outfile)
+	elif A.type in[Y,'hinakoi_card','sakukoi_movie','hinakoi_movie']:
+		try:M(A.type,A.memberdata,A.catalog,A.pathserver,A.pathlocal,E(A.fromindex),E(A.toindex))
+		except KeyboardInterrupt:D('Process Cancelled by User');P(1)
